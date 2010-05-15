9ce3857e35ad2890528852e83f961f2f1e45eb99
Michael Staib <michael.staib@infinitelyeven.net>
2010-05-15 04:23:43 -0400

    Character scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Observatory::Application.routes.draw do |map|
+  resources :characters
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20100515082335_create_characters.rb
@@ -0,0 +1,15 @@
+class CreateCharacters < ActiveRecord::Migration
+  def self.up
+    create_table :characters do |t|
+      t.string :name
+      t.text :description
+      t.string :icon
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :characters
+  end
+end
