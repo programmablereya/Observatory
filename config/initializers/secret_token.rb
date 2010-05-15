38cd77c928906f8eb2ca4c2e0f92ccf41575b2e9
Michael Staib <michael.staib@infinitelyeven.net>
2010-05-15 03:46:21 -0400

    Set up Rails hierarchy.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '3717a2b4016d20464b5fc895d0a8eedf4c0342c6e775de7462affeac7bc2c92e1ced7614b86b2e305c7e0b7593e2d5099afb4d11771977eadbe5c20f48069ee6'
